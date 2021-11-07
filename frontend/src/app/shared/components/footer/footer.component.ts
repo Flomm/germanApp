9e2782d983b15936326bcc842c655f9cc457a705
Florian Baranya <florian.baranya@gmail.com>
2021-11-07 11:56:22 +0100

    Translate shared comps+not found page

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  date: string = new Date().getFullYear().toString();
+}
