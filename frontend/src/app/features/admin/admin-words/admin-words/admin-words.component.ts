34d3359260f0faf2cd93265f727b4563bd0a502d
baranyaf <baranya.florian@molaris.hu>
2021-11-24 19:59:43 +0100

    Wordlist is working

--- frontend/src/app/features/admin/admin-words/admin-words/admin-words.component.ts
+++ frontend/src/app/features/admin/admin-words/admin-words/admin-words.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WordService } from 'src/app/core/services/wordService/word.service';
+import { Language } from 'src/app/shared/models/enums/Language.enum';
+import IGetWordResponse from 'src/app/shared/models/responses/IGetWordsResponse';
 
 @Component({
   selector: 'app-admin-words',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-words.component.scss']
 })
 export class AdminWordsComponent implements OnInit {
+  getWordResponse: IGetWordResponse;
 
-  constructor() { }
+  constructor(private wordService: WordService) {}
 
   ngOnInit(): void {
+    this.getWordData();
   }
 
+  getWordData(): void {
+    this.wordService
+      .getAllWords(Language.DE)
+      .subscribe((response) => (this.getWordResponse = response));
+  }
 }
--- frontend/src/app/features/admin/admin-words/words-list-table/words-list-table.component.ts
+++ frontend/src/app/features/admin/admin-words/words-list-table/words-list-table.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import IGetWordData from 'src/app/shared/models/models/viewModels/IGetWordData.viewModel';
+import IGetWordResponse from 'src/app/shared/models/responses/IGetWordsResponse';
 
 @Component({
   selector: 'app-words-list-table',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./words-list-table.component.scss']
 })
 export class WordsListTableComponent implements OnInit {
+  private paginator: MatPaginator;
+  displayedColumns: string[] = ['word', 'info', 'delete'];
+  dataSource: MatTableDataSource<IGetWordData>;
 
-  constructor() { }
+  @Input()
+  getWordResponse: IGetWordResponse;
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.dataSource.paginator = this.paginator;
   }
 
-}
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<IGetWordData>([]);
+  }
+
+  ngOnChanges() {
+    if (this.getWordResponse) {
+      this.dataSource = new MatTableDataSource<IGetWordData>(
+        this.getWordResponse.wordList
+      );
+    }
+  }
+}  
