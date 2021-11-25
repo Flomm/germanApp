24d4027a1bee43e0d27283f59c8e556faefbf2bf
baranyaf <baranya.florian@molaris.hu>
2021-11-25 20:55:36 +0100

    Add removeWord to word service

--- frontend/src/app/core/services/wordService/word.service.ts
+++ frontend/src/app/core/services/wordService/word.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { Language } from 'src/app/shared/models/enums/Language.enum';
+import { ICustomResponse } from 'src/app/shared/models/responses/ICustomResponse';
 import IGetWordResponse from 'src/app/shared/models/responses/IGetWordsResponse';
 import { environment } from 'src/environments/environment';
 
@@ -19,6 +20,25 @@ export class WordService {
         catchError((httpError) =>
           of({
             wordList: [],
+            message: httpError.error.message ?? 'Connection failure',
+            isError: true,
+          })
+        )
+      );
+  }
+
+  removeWord(lang: Language, wordId: number): Observable<ICustomResponse> {
+    return this.httpClient
+      .delete<ICustomResponse>(`${environment.serverUrl}/word/${lang}/${wordId}`)
+      .pipe(
+        map((response) => {
+          return {
+            message: response.message,
+            isError: false,
+          };
+        }),
+        catchError((httpError) =>
+          of({
             message: httpError.error.message ?? 'Connection failure',
             isError: true,
           })
