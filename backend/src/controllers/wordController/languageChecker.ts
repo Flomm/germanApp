ad4e7809f4a194cb2c7d0edf2d1673fc1108858c
baranyaf <baranya.florian@molaris.hu>
2021-11-27 21:07:44 +0100

    add language checker helper function

---
+++ backend/src/controllers/wordController/languageChecker.ts
@@ -0,0 +1,5 @@
+import { Language } from "../../models/models/Enums/Language.enum";
+
+export default function languageChecker (lang: Language): boolean {
+    return !(<any>Object).values(Language).includes(lang) 
+}
