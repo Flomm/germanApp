03edeef9348c69bed135bee0681697eec226b808
baranyaf <baranya.florian@molaris.hu>
2021-12-05 22:22:53 +0100

    Remove id from datamodel

--- backend/src/models/models/dataModels/IAddWordDataModel.ts
+++ backend/src/models/models/dataModels/IAddWordDataModel.ts
@@ -2,7 +2,6 @@ import { Gender } from "../Enums/Gender.enum";
 import IAddTranslationWordDataModel from "./IAddTranslationWordDataModel";
 
 export default interface IAddWordDataModel {
-    id: number,
     word: string,
     translations: IAddTranslationWordDataModel[],
     gender?: Gender,
