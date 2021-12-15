036a703ab81c69ee6fd0fc4cf763bad77962f2a1
baranyaf <baranya.florian@molaris.hu>
2021-12-15 23:44:42 +0100

    Add tests backend tests for addWord

--- backend/src/controllers/wordController/wordController.test.ts
+++ backend/src/controllers/wordController/wordController.test.ts
@@ -4,161 +4,243 @@ import { jwtService } from '../../services/jwtService/jwt.service';
 import { wordService } from '../../services/wordService/wordService';
 import IGetWordsDataModel from '../../models/models/dataModels/IGetWordsDataModel';
 import { serverError } from '../../services/errorCreatorService/errorCreator.service';
+import IAddWordDataModel from '../../models/models/dataModels/IAddWordDataModel';
+import { Gender } from '../../models/models/Enums/Gender.enum';
+import { Language } from '../../models/models/Enums/Language.enum';
 
 const mockDeWords: IGetWordsDataModel[] = [
   {
     id: 1,
-    word: 'Wasser'
+    word: 'Wasser',
   },
 ];
 
 const mockHunWords: IGetWordsDataModel[] = [
-    {
-      id: 1,
-      word: 'víz'
-    },
-  ];
+  {
+    id: 1,
+    word: 'víz',
+  },
+];
+
+const mockDeWord: IAddWordDataModel = {
+  word: 'Spiegel',
+  gender: Gender.DER,
+  translations: [{ translation: 'tükör' }],
+};
+
+const mockHuWord: IAddWordDataModel = {
+  word: 'tükör',
+  translations: [{ translation: 'Spiegel', gender: Gender.DER }],
+};
 
 const token: string =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsInJvbGVJZCI6MSwiaWF0IjoxNjI1ODU2Mzc5LCJleHAiOjE2MjU4NTk5Nzl9.q1O5nZgju0sO-ORTxiO745KkofE7nnFr0YsMML6Uuas';
 
 describe('GET /', () => {
-    test('succesfully retrieved german words', async () => {
-      //Arrange
-      wordService.getAllWords = jest.fn().mockResolvedValue(mockDeWords);
-      jwtService.verifyToken = jest.fn().mockResolvedValue(true);
-  
-      //Act
-      const response = await request(app)
-        .get('/api/word/de')
-        .set({ authorization: `Bearer ${token}` });
-  
-      //Assert
-      expect(response.statusCode).toEqual(200);
-      expect(response.body).toEqual({ wordList: mockDeWords });
-      expect(wordService.getAllWords).toHaveBeenCalledTimes(1);
-    });
+  test('succesfully retrieved german words', async () => {
+    //Arrange
+    wordService.getAllWords = jest.fn().mockResolvedValue(mockDeWords);
+    jwtService.verifyToken = jest.fn().mockResolvedValue(true);
 
-    test('succesfully retrieved hungarian words', async () => {
-        //Arrange
-        wordService.getAllWords = jest.fn().mockResolvedValue(mockHunWords);
-        jwtService.verifyToken = jest.fn().mockResolvedValue(true);
-    
-        //Act
-        const response = await request(app)
-          .get('/api/word/hu')
-          .set({ authorization: `Bearer ${token}` });
-    
-        //Assert
-        expect(response.statusCode).toEqual(200);
-        expect(response.body).toEqual({ wordList: mockHunWords });
-        expect(wordService.getAllWords).toHaveBeenCalledTimes(1);
-      });
+    //Act
+    const response = await request(app)
+      .get('/api/word/de')
+      .set({ authorization: `Bearer ${token}` });
 
-      test('invalid language', async () => {
-        //Arrange
-        console.error = jest.fn();
-    
-        //Act
-        const response = await request(app)
-          .get('/api/word/lol')
-          .set({ authorization: `Bearer ${token}` });
-    
-        //Assert
-        expect(response.statusCode).toEqual(400);
-        expect(response.body).toEqual({ message: 'Nincs ilyen nyelv a szótárban.' });
-      });
+    //Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({ wordList: mockDeWords });
+    expect(wordService.getAllWords).toHaveBeenCalledTimes(1);
+  });
 
-  
-    test('error in the service', async () => {
-      //Arrange
-      wordService.getAllWords = jest.fn().mockRejectedValue(serverError('test'));
-      console.error = jest.fn();
-  
-      //Act
-      const response = await request(app)
-        .get('/api/word/de')
-        .set({ authorization: `Bearer ${token}` });
-  
-      //Assert
-      expect(response.statusCode).toEqual(500);
-      expect(response.body).toEqual({ message: 'test' });
-      expect(wordService.getAllWords).toHaveBeenCalledTimes(1);
+  test('succesfully retrieved hungarian words', async () => {
+    //Arrange
+    wordService.getAllWords = jest.fn().mockResolvedValue(mockHunWords);
+    jwtService.verifyToken = jest.fn().mockResolvedValue(true);
+
+    //Act
+    const response = await request(app)
+      .get('/api/word/hu')
+      .set({ authorization: `Bearer ${token}` });
+
+    //Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({ wordList: mockHunWords });
+    expect(wordService.getAllWords).toHaveBeenCalledTimes(1);
+  });
+
+  test('invalid language', async () => {
+    //Arrange
+    console.error = jest.fn();
+
+    //Act
+    const response = await request(app)
+      .get('/api/word/lol')
+      .set({ authorization: `Bearer ${token}` });
+
+    //Assert
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({
+      message: 'Nincs ilyen nyelv a szótárban.',
     });
   });
 
-  describe('DELETE /word', () => {
-    const idRequest = '1';
-  
-    test('successfully delete german word', async () => {
-      //Arrange
-      wordService.removeWord = jest.fn().mockResolvedValue(idRequest);
-  
-      //Act
-      const response = await request(app)
-        .delete('/api/word/de/1')
-        .set({ authorization: `Bearer ${token}` })
-        .send(idRequest);
-  
-      //Assert
-      expect(response.statusCode).toEqual(200);
-      expect(response.body).toEqual({
-        message: 'A szó sikeresen eltávolítva.',
-      });
-      expect(wordService.removeWord).toHaveBeenLastCalledWith(
-        parseInt(idRequest), 'de'
-      );
-    });
+  test('error in the service', async () => {
+    //Arrange
+    wordService.getAllWords = jest.fn().mockRejectedValue(serverError('test'));
+    console.error = jest.fn();
 
-    test('successfully delete hungarian word', async () => {
-      //Arrange
-      wordService.removeWord = jest.fn().mockResolvedValue(idRequest);
-  
-      //Act
-      const response = await request(app)
-        .delete('/api/word/hu/1')
-        .set({ authorization: `Bearer ${token}` })
-        .send(idRequest);
-  
-      //Assert
-      expect(response.statusCode).toEqual(200);
-      expect(response.body).toEqual({
-        message: 'A szó sikeresen eltávolítva.',
-      });
-      expect(wordService.removeWord).toHaveBeenLastCalledWith(
-        parseInt(idRequest), 'hu'
-      );
-    });
+    //Act
+    const response = await request(app)
+      .get('/api/word/de')
+      .set({ authorization: `Bearer ${token}` });
 
-    test('invalid word id', async () => {
-      //Act
-      const response = await request(app)
-        .delete('/api/word/hu/azaz')
-        .set({ authorization: `Bearer ${token}` })
-        .send(idRequest);
-  
-      //Assert
-      expect(response.statusCode).toEqual(400);
-      expect(response.body).toEqual({
-        message: 'A szó id pozitív egész szám kell legyen.',
-      });
+    //Assert
+    expect(response.statusCode).toEqual(500);
+    expect(response.body).toEqual({ message: 'test' });
+    expect(wordService.getAllWords).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('DELETE /word', () => {
+  const idRequest = '1';
+
+  test('successfully delete german word', async () => {
+    //Arrange
+    wordService.removeWord = jest.fn().mockResolvedValue(idRequest);
+
+    //Act
+    const response = await request(app)
+      .delete('/api/word/de/1')
+      .set({ authorization: `Bearer ${token}` })
+      .send(idRequest);
+
+    //Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      message: 'A szó sikeresen eltávolítva.',
     });
-  
-    test('error in the service', async () => {
-      //Arrange
-      wordService.removeWord = jest
-        .fn()
-        .mockRejectedValue(serverError('error'));
-      console.error = jest.fn();
-      //Act
-      const response = await request(app)
-        .delete('/api/word/hu/1')
-        .set({ authorization: `Bearer ${token}` })
-        .send(idRequest);
-  
-      //Assert
-      expect(response.statusCode).toEqual(500);
-      expect(response.body).toEqual({ message: 'error' });
+    expect(wordService.removeWord).toHaveBeenLastCalledWith(
+      parseInt(idRequest),
+      'de',
+    );
+  });
+
+  test('successfully delete hungarian word', async () => {
+    //Arrange
+    wordService.removeWord = jest.fn().mockResolvedValue(idRequest);
+
+    //Act
+    const response = await request(app)
+      .delete('/api/word/hu/1')
+      .set({ authorization: `Bearer ${token}` })
+      .send(idRequest);
+
+    //Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      message: 'A szó sikeresen eltávolítva.',
     });
-  }) 
-  
+    expect(wordService.removeWord).toHaveBeenLastCalledWith(
+      parseInt(idRequest),
+      'hu',
+    );
+  });
+
+  test('invalid word id', async () => {
+    //Act
+    const response = await request(app)
+      .delete('/api/word/hu/azaz')
+      .set({ authorization: `Bearer ${token}` })
+      .send(idRequest);
+
+    //Assert
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({
+      message: 'A szó id pozitív egész szám kell legyen.',
+    });
+  });
+
+  test('error in the service', async () => {
+    //Arrange
+    wordService.removeWord = jest.fn().mockRejectedValue(serverError('error'));
+    console.error = jest.fn();
+    //Act
+    const response = await request(app)
+      .delete('/api/word/hu/1')
+      .set({ authorization: `Bearer ${token}` })
+      .send(idRequest);
+
+    //Assert
+    expect(response.statusCode).toEqual(500);
+    expect(response.body).toEqual({ message: 'error' });
+  });
+});
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+describe('POST /ticket', () => {
+  beforeEach(() => {
+    jwtService.verifyToken = jest.fn().mockResolvedValue(true);
+  });
+
+  test('successfully adding german word', async () => {
+    //Arrange
+    wordService.addNewWord = jest.fn().mockResolvedValue(Promise.resolve);
+
+    //Act
+    const response = await request(app)
+      .post('/api/word/de')
+      .set({ authorization: `Bearer ${token}` })
+      .send(mockDeWord);
+
+    //Assert
+    expect(response.statusCode).toEqual(201);
+    expect(response.body).toEqual({
+      message: 'Szó sikeresen hozzáadva.',
+    });
+    expect(wordService.addNewWord).toHaveBeenCalledWith(
+      Language.DE,
+      mockDeWord,
+    );
+  });
+
+  test('successfully adding hungarian word', async () => {
+    //Arrange
+    wordService.addNewWord = jest.fn().mockResolvedValue(Promise.resolve);
+
+    //Act
+    const response = await request(app)
+      .post('/api/word/hu')
+      .set({ authorization: `Bearer ${token}` })
+      .send(mockHuWord);
+
+    //Assert
+    expect(response.statusCode).toEqual(201);
+    expect(response.body).toEqual({
+      message: 'Szó sikeresen hozzáadva.',
+    });
+    expect(wordService.addNewWord).toHaveBeenCalledWith(
+      Language.HU,
+      mockHuWord,
+    );
+  });
+
+  test('error in the service', async () => {
+    //Arrange
+    wordService.addNewWord = jest.fn().mockRejectedValue(serverError('test'));
+    console.error = jest.fn();
+    //Act
+    const response = await request(app)
+      .post('/api/word/de')
+      .set({ authorization: `Bearer ${token}` })
+      .send(mockDeWord);
+
+    //Assert
+    expect(response.statusCode).toEqual(500);
+    expect(response.body).toEqual({ message: 'test' });
+    expect(wordService.addNewWord).toHaveBeenCalledWith(
+      Language.DE,
+      mockDeWord,
+    );
+  });
+});
--- backend/src/services/wordService/wordService.test.ts
+++ backend/src/services/wordService/wordService.test.ts
@@ -1,113 +1,212 @@
-import IDbResultDataModel from "../../models/models/dataModels/IDbResultDataModel";
-import IGetWordsDataModel from "../../models/models/dataModels/IGetWordsDataModel";
-import { Language } from "../../models/models/Enums/Language.enum";
-import { wordRepository } from "../../repository/wordRepository/wordRepository";
-import { notFoundError, serverError } from "../errorCreatorService/errorCreator.service";
-import { wordService } from "./wordService";
+import IAddWordDataModel from '../../models/models/dataModels/IAddWordDataModel';
+import IDbResultDataModel from '../../models/models/dataModels/IDbResultDataModel';
+import IGetWordsDataModel from '../../models/models/dataModels/IGetWordsDataModel';
+import { Gender } from '../../models/models/Enums/Gender.enum';
+import { Language } from '../../models/models/Enums/Language.enum';
+import { wordRepository } from '../../repository/wordRepository/wordRepository';
+import {
+  notFoundError,
+  serverError,
+} from '../errorCreatorService/errorCreator.service';
+import { wordService } from './wordService';
 
 const mockDeWords: IGetWordsDataModel[] = [
-    {
-      id: 1,
-      word: 'Wasser'
-    },
-  ];
-  
-  const mockHunWords: IGetWordsDataModel[] = [
-      {
-        id: 1,
-        word: 'víz'
-      },
-    ];
-  
-  const mockLanguage: Language = Language.DE
+  {
+    id: 1,
+    word: 'Wasser',
+  },
+];
+
+const mockHunWords: IGetWordsDataModel[] = [
+  {
+    id: 1,
+    word: 'víz',
+  },
+];
+
+const mockDeWord: IAddWordDataModel = {
+  word: 'Spiegel',
+  gender: Gender.DER,
+  translations: [{ translation: 'tükör' }],
+};
+
+const mockHuWord: IAddWordDataModel = {
+  word: 'tükör',
+  translations: [{ translation: 'Spiegel', gender: Gender.DER }],
+};
+
+const mockLanguage: Language = Language.DE;
 
 describe('getAllWords', () => {
-    test('successfully retrieved german words', async () => {
-      //Arrange
-      wordRepository.getAllWords = jest.fn().mockResolvedValue(mockDeWords);
-  
-      //Act
-      const words: IGetWordsDataModel[] = await wordService.getAllWords(mockLanguage);
-  
-      //Assert
-      expect(words).toStrictEqual(mockDeWords);
-      expect(wordRepository.getAllWords).toHaveBeenCalledTimes(1);
-    });
+  test('successfully retrieved german words', async () => {
+    //Arrange
+    wordRepository.getAllWords = jest.fn().mockResolvedValue(mockDeWords);
 
-    test('successfully retrieved hungarian words', async () => {
-        //Arrange
-        wordRepository.getAllWords = jest.fn().mockResolvedValue(mockHunWords);
-    
-        //Act
-        const words: IGetWordsDataModel[] = await wordService.getAllWords(mockLanguage);
-    
-        //Assert
-        expect(words).toStrictEqual(mockHunWords);
-        expect(wordRepository.getAllWords).toHaveBeenCalledTimes(1);
-      });
+    //Act
+    const words: IGetWordsDataModel[] = await wordService.getAllWords(
+      mockLanguage,
+    );
 
-
-
-    test('repository error', async () => {
-      //Arrange
-      wordRepository.getAllWords = jest
-        .fn()
-        .mockRejectedValue(serverError('test'));
-  
-      //Act
-      try {
-        await wordService.getAllWords(Language.DE);
-      } catch (err) {
-        //Assert
-        expect(err).toEqual(serverError('test'));
-        expect(wordRepository.getAllWords).toHaveBeenCalledTimes(1);
-      }
-    });
+    //Assert
+    expect(words).toStrictEqual(mockDeWords);
+    expect(wordRepository.getAllWords).toHaveBeenCalledTimes(1);
   });
 
-  describe('removeWord', () => {
-    const idRequest = 1;
+  test('successfully retrieved hungarian words', async () => {
+    //Arrange
+    wordRepository.getAllWords = jest.fn().mockResolvedValue(mockHunWords);
 
-    test('successfully remove word', async () => {
-      wordRepository.removeWord = jest.fn().mockResolvedValue(idRequest);
-  
-      //Act
-      await wordService.removeWord(idRequest, mockLanguage);
-  
+    //Act
+    const words: IGetWordsDataModel[] = await wordService.getAllWords(
+      mockLanguage,
+    );
+
+    //Assert
+    expect(words).toStrictEqual(mockHunWords);
+    expect(wordRepository.getAllWords).toHaveBeenCalledTimes(1);
+  });
+
+  test('repository error', async () => {
+    //Arrange
+    wordRepository.getAllWords = jest
+      .fn()
+      .mockRejectedValue(serverError('test'));
+
+    //Act
+    try {
+      await wordService.getAllWords(Language.DE);
+    } catch (err) {
       //Assert
-      expect(wordRepository.removeWord).toHaveBeenCalledWith(idRequest, mockLanguage);
-    });
-  
-    test('WordId not found', async () => {
-      //Arrange
-      const mockDbResult: IDbResultDataModel = {
-        affectedRows: 0,
-      };
-      wordRepository.removeWord = jest.fn().mockResolvedValue(mockDbResult);
-  
-      //Act
-      try {
-        await wordService.removeWord(idRequest, mockLanguage);
-      } catch (err) {
-        //Assert
-        expect(wordRepository.removeWord).toHaveBeenCalledWith(idRequest, mockLanguage);
-        expect(err).toEqual(notFoundError('A szó id nem található.'));
-      }
-    })
+      expect(err).toEqual(serverError('test'));
+      expect(wordRepository.getAllWords).toHaveBeenCalledTimes(1);
+    }
+  });
+});
 
-    test('repository error', async () => {
-      //Arrange
-      wordRepository.removeWord = jest
-        .fn()
-        .mockRejectedValue(serverError('test'));
-  
-      //Act
-      try {
-        await wordService.removeWord(idRequest, Language.DE);
-      } catch (err) {
-        //Assert
-        expect(err).toEqual(serverError('test'));
-        expect(wordRepository.getAllWords).toHaveBeenCalledTimes(1);
-      }
-    });
-  })
+describe('removeWord', () => {
+  const idRequest = 1;
+
+  test('successfully remove word', async () => {
+    wordRepository.removeWord = jest.fn().mockResolvedValue(idRequest);
+
+    //Act
+    await wordService.removeWord(idRequest, mockLanguage);
+
+    //Assert
+    expect(wordRepository.removeWord).toHaveBeenCalledWith(
+      idRequest,
+      mockLanguage,
+    );
+  });
+
+  test('WordId not found', async () => {
+    //Arrange
+    const mockDbResult: IDbResultDataModel = {
+      affectedRows: 0,
+    };
+    wordRepository.removeWord = jest.fn().mockResolvedValue(mockDbResult);
+
+    //Act
+    try {
+      await wordService.removeWord(idRequest, mockLanguage);
+    } catch (err) {
+      //Assert
+      expect(wordRepository.removeWord).toHaveBeenCalledWith(
+        idRequest,
+        mockLanguage,
+      );
+      expect(err).toEqual(notFoundError('A szó id nem található.'));
+    }
+  });
+
+  test('repository error', async () => {
+    //Arrange
+    wordRepository.removeWord = jest
+      .fn()
+      .mockRejectedValue(serverError('test'));
+
+    //Act
+    try {
+      await wordService.removeWord(idRequest, mockLanguage);
+    } catch (err) {
+      //Assert
+      expect(err).toEqual(serverError('test'));
+      expect(wordRepository.getAllWords).toHaveBeenCalledTimes(1);
+    }
+  });
+});
+
+describe('addNewWord', () => {
+  test('successfully adding german word', async () => {
+    //Arrange
+    const mockDbResult: IDbResultDataModel = {
+      affectedRows: 1,
+    };
+    wordRepository.addNewWordEntry = jest.fn().mockResolvedValue(mockDbResult);
+
+    //Act
+    await wordService.addNewWord(mockLanguage, mockDeWord);
+
+    //Assert
+    expect(wordRepository.addNewWordEntry).toHaveBeenCalledWith(
+      Language.DE,
+      mockDeWord,
+    );
+  });
+
+  test('successfully adding hungarian word', async () => {
+    //Arrange
+    const mockDbResult: IDbResultDataModel = {
+      affectedRows: 1,
+    };
+    wordRepository.addNewWordEntry = jest.fn().mockResolvedValue(mockDbResult);
+
+    //Act
+    await wordService.addNewWord(Language.HU, mockDeWord);
+
+    //Assert
+    expect(wordRepository.addNewWordEntry).toHaveBeenCalledWith(
+      Language.HU,
+      mockDeWord,
+    );
+  });
+
+  test('should create servererror if affectedrows is 0', async () => {
+    //Arrange
+    const mockDbResult: IDbResultDataModel = {
+      affectedRows: 0,
+    };
+    wordRepository.addNewWordEntry = jest.fn().mockResolvedValue(mockDbResult);
+
+    //Act
+    try {
+      await wordService.addNewWord(mockLanguage, mockDeWord);
+    } catch (err) {
+      //Assert
+      expect(err).toEqual(notFoundError('A hozzáadás nem sikerült.'));
+      expect(wordRepository.addNewWordEntry).toHaveBeenCalledWith(
+        Language.DE,
+        mockDeWord,
+      );
+    }
+  });
+
+  test('repository error', async () => {
+    //Arrange
+    wordRepository.addNewWordEntry = jest
+      .fn()
+      .mockRejectedValue(serverError('test'));
+
+    //Act
+    try {
+      await wordService.addNewWord(mockLanguage, mockDeWord);
+    } catch (err) {
+      //Assert
+      expect(err).toEqual(serverError('test'));
+      expect(wordRepository.addNewWordEntry).toHaveBeenCalledWith(
+        Language.DE,
+        mockDeWord,
+      );
+    }
+  });
+});
