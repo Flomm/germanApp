ca75013641e739f594c8d2bf0d7b4297f9db6097
baranyaf <baranya.florian@molaris.hu>
2021-12-24 12:18:42 +0100

    Fix property type - array

--- backend/src/models/responses/IGetTranslationsResponse.ts
+++ backend/src/models/responses/IGetTranslationsResponse.ts
@@ -1,5 +1,5 @@
 import ITranslationDataModel from '../models/dataModels/ITranslationDataModel';
 
 export default interface IGetTranslationsResponse {
-  translationList: ITranslationDataModel;
+  translationList: ITranslationDataModel[];
 }
