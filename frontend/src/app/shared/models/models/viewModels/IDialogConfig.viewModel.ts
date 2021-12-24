df277ec63093eb6fa8bb207054f32df59cea29ef
baranyaf <baranya.florian@molaris.hu>
2021-12-24 23:56:36 +0100

    Modify data added to dialog

--- frontend/src/app/shared/models/models/viewModels/IDialogConfig.viewModel.ts
+++ frontend/src/app/shared/models/models/viewModels/IDialogConfig.viewModel.ts
@@ -1,7 +1,10 @@
+import IModifyWordDialogData from './IModifyWordDialogData.viewModel';
+
 export default interface IDialogConfig {
   isCancelButtonVisible?: boolean;
   cancelButtonText?: string;
   okButtonText?: string;
   isClosable?: boolean;
   dialogText: string;
+  modifyWordData: IModifyWordDialogData;
 }
