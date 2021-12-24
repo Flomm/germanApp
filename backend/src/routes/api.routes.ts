a3c311d07f4d5b0a3db6208dee16fd7fb3f255b2
baranyaf <baranya.florian@molaris.hu>
2021-12-24 11:41:38 +0100

    Add translation route

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import { userRouter } from './user.api.routes';
 import { wordRouter } from './word.api.routes';
+import { translationRouter } from './translation.api.routes';
 
 const apiRouter = express.Router();
 apiRouter.use(cors());
 apiRouter.use(express.json());
 apiRouter.use('/user', userRouter);
 apiRouter.use('/word', wordRouter);
+apiRouter.use('/translation', translationRouter);
 
 export default apiRouter;
