3c514e604a757a055e4c742bf83e2319d3c6eb2e
baranyaf <baranya.florian@molaris.hu>
2021-12-29 16:01:04 +0100

    Add welcome page to admin routing

--- frontend/src/app/features/admin/admin-main/admin-main/admin-main.component.ts
+++ frontend/src/app/features/admin/admin-main/admin-main/admin-main.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-main',
   templateUrl: './admin-main.component.html',
   styleUrls: ['./admin-main.component.scss'],
 })
-export class AdminMainComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class AdminMainComponent {}
