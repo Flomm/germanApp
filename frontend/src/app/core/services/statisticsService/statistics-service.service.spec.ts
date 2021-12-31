fe1f08871217deb8b6d590f3220a1d48bf6043e6
baranyaf <baranya.florian@molaris.hu>
2021-12-31 13:17:24 +0100

    Create statistic service

---
+++ frontend/src/app/core/services/statisticsService/statistics-service.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { StatisticsService } from './statistics-service.service';
+
+describe('StatisticsServiceService', () => {
+  let service: StatisticsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(StatisticsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/core/services/statisticsService/statistics-service.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import IGetStatisticsResponse from 'src/app/shared/models/responses/IGetStatisticsResponse';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StatisticsService {
+  constructor(private httpClient: HttpClient) {}
+
+  getMyStatistics(): Observable<IGetStatisticsResponse> {
+    return this.httpClient
+      .get<IGetStatisticsResponse>(`${environment.serverUrl}/myStatistics/`)
+      .pipe(
+        catchError((httpError) =>
+          of({
+            message: httpError.error.message ?? 'Connection failure',
+            isError: true,
+          })
+        )
+      );
+  }
+}
