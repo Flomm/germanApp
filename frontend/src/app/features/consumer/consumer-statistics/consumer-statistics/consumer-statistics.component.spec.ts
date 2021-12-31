b106df3224d73456b7c80c11d0b9ac1203d39175
baranyaf <baranya.florian@molaris.hu>
2021-12-31 13:40:26 +0100

    Add test

--- frontend/src/app/features/consumer/consumer-statistics/consumer-statistics/consumer-statistics.component.spec.ts
+++ frontend/src/app/features/consumer/consumer-statistics/consumer-statistics/consumer-statistics.component.spec.ts
@@ -1,24 +1,51 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { StatisticsService } from 'src/app/core/services/statisticsService/statistics-service.service';
+import IGetStatisticsResponse from 'src/app/shared/models/responses/IGetStatisticsResponse';
 import { ConsumerStatisticsComponent } from './consumer-statistics.component';
 
 describe('ConsumerStatisticsComponent', () => {
   let component: ConsumerStatisticsComponent;
   let fixture: ComponentFixture<ConsumerStatisticsComponent>;
+  let statisticsServiceSpy: jasmine.SpyObj<StatisticsService>;
 
   beforeEach(async () => {
+    statisticsServiceSpy = jasmine.createSpyObj<StatisticsService>(
+      'statisticsService',
+      ['getMyStatistics']
+    );
     await TestBed.configureTestingModule({
       declarations: [ConsumerStatisticsComponent],
+      providers: [
+        { provide: StatisticsService, useValue: statisticsServiceSpy },
+      ],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ConsumerStatisticsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getMyStatistics on initialization and make the getStatisticsResponse equal to the response', () => {
+    //Arrange
+    const mockResponse: IGetStatisticsResponse = {
+      message: 'ok',
+      isError: true,
+    };
+    statisticsServiceSpy.getMyStatistics.and.returnValue(of(mockResponse));
+
+    //Act
+    fixture.detectChanges();
+
+    //Assert
+    expect(statisticsServiceSpy.getMyStatistics).toHaveBeenCalledTimes(1);
+    expect(component.getStatisticsResponse).toEqual(mockResponse);
   });
 });
