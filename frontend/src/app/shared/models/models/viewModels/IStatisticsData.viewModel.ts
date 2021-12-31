f34827abfd71f4c67de9f363c19d5347e437eb8e
baranyaf <baranya.florian@molaris.hu>
2021-12-31 12:57:25 +0100

    Add models for statistics

---
+++ frontend/src/app/shared/models/models/viewModels/IStatisticsData.viewModel.ts
@@ -0,0 +1,7 @@
+export default interface IStatisticsData {
+  userId: number;
+  numOfStartedGames: number;
+  numOfFinishedGames: number;
+  numOfCorrectAnswers: number;
+  numOfIncorrectAnswers: number;
+}
