13a9b85a4f6361894ffee3b76ded26e8da98a00f
baranyaf <baranya.florian@molaris.hu>
2022-01-02 15:32:05 +0100

    Add tests for increment data

--- backend/src/controllers/statisticsController/statisticsController.test.ts
+++ backend/src/controllers/statisticsController/statisticsController.test.ts
@@ -4,9 +4,12 @@ import IStatisticsDomainModel from '../../models/models/domainModels/IStatistics
 import { serverError } from '../../services/errorCreatorService/errorCreator.service';
 import { statisticsService } from '../../services/statisticsService/statisticsService';
 import { jwtService } from '../../services/jwtService/jwt.service';
+import { StatDataType } from '../../models/models/Enums/StatDataType.enum';
 
 const token: string =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsInJvbGVJZCI6MSwiaWF0IjoxNjI1ODU2Mzc5LCJleHAiOjE2MjU4NTk5Nzl9.q1O5nZgju0sO-ORTxiO745KkofE7nnFr0YsMML6Uuas';
+
+const mockUserId: string = '1';
 
 describe('GET /my-statistics', () => {
   const mockStatisticsData: IStatisticsDomainModel = {
@@ -16,7 +19,6 @@ describe('GET /my-statistics', () => {
     numOfFinishedGames: 0,
     numOfStartedGames: 0,
   };
-  const mockUserId: string = '1';
 
   test('succesfully retrieved statistics data', async () => {
     //Arrange
@@ -53,5 +55,107 @@ describe('GET /my-statistics', () => {
     expect(response.statusCode).toEqual(500);
     expect(response.body).toEqual({ message: 'test' });
     expect(statisticsService.getMyStatistics).toHaveBeenCalledWith(mockUserId);
+  });
+});
+
+describe('PUT /increment/:dataType', () => {
+  test('successfully updating SG stat data', async () => {
+    //Arrange
+    statisticsService.incrementStatData = jest
+      .fn()
+      .mockResolvedValue(Promise.resolve);
+
+    //Act
+    const response = await request(app)
+      .put('/api/statistics/increment/1')
+      .set({ authorization: `Bearer ${token}` });
+
+    //Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      message: 'Adat sikeresen módosítva.',
+    });
+    expect(statisticsService.incrementStatData).toHaveBeenCalledWith(
+      mockUserId,
+      StatDataType.SG,
+    );
+  });
+
+  test('successfully updating AC stat data', async () => {
+    //Arrange
+    statisticsService.incrementStatData = jest
+      .fn()
+      .mockResolvedValue(Promise.resolve);
+
+    //Act
+    const response = await request(app)
+      .put('/api/statistics/increment/3')
+      .set({ authorization: `Bearer ${token}` });
+
+    //Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      message: 'Adat sikeresen módosítva.',
+    });
+    expect(statisticsService.incrementStatData).toHaveBeenCalledWith(
+      mockUserId,
+      StatDataType.CA,
+    );
+  });
+
+  test('should send back 400 error if params.dataType is not an integer', async () => {
+    //Arrange
+    statisticsService.incrementStatData = jest
+      .fn()
+      .mockResolvedValue(Promise.resolve);
+
+    //Act
+    const response = await request(app)
+      .put('/api/statistics/increment/fail')
+      .set({ authorization: `Bearer ${token}` });
+
+    //Assert
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({
+      message: 'Érvénytelen adattípus azonosító.',
+    });
+  });
+
+  test('should send back 400 error if params.dataType is not in range of 1-4', async () => {
+    //Arrange
+    statisticsService.incrementStatData = jest
+      .fn()
+      .mockResolvedValue(Promise.resolve);
+
+    //Act
+    const response = await request(app)
+      .put('/api/statistics/increment/15')
+      .set({ authorization: `Bearer ${token}` });
+
+    //Assert
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({
+      message: 'Érvénytelen adattípus azonosító.',
+    });
+  });
+
+  test('error in the service', async () => {
+    //Arrange
+    statisticsService.incrementStatData = jest
+      .fn()
+      .mockRejectedValue(serverError('test'));
+
+    //Act
+    const response = await request(app)
+      .put('/api/statistics/increment/1')
+      .set({ authorization: `Bearer ${token}` });
+
+    //Assert
+    expect(response.statusCode).toEqual(500);
+    expect(response.body).toEqual({ message: 'test' });
+    expect(statisticsService.incrementStatData).toHaveBeenCalledWith(
+      mockUserId,
+      StatDataType.SG,
+    );
   });
 });
