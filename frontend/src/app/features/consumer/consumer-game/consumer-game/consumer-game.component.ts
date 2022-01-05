399e481e81b44201eeae8a3554c74b9c695b2b88
baranyaf <baranya.florian@molaris.hu>
2022-01-05 21:30:20 +0100

    Add response holder

--- frontend/src/app/features/consumer/consumer-game/consumer-game/consumer-game.component.ts
+++ frontend/src/app/features/consumer/consumer-game/consumer-game/consumer-game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from 'src/app/core/services/gameService/game.service';
+import IGetWordData from 'src/app/shared/models/models/viewModels/IGetWordData.viewModel';
 import IGetRandomWordRequest from 'src/app/shared/models/requests/IGetRandomWordRequest';
 
 @Component({
@@ -8,13 +9,21 @@ import IGetRandomWordRequest from 'src/app/shared/models/requests/IGetRandomWord
   styleUrls: ['./consumer-game.component.scss'],
 })
 export class ConsumerGameComponent implements OnInit {
+  isGameOn = false;
+  listOfWords: IGetWordData[] = [];
+  errorMessage: string;
+
   constructor(private gameService: GameService) {}
 
   ngOnInit(): void {}
 
   onGetRandomWords(randomWordRequestData: IGetRandomWordRequest): void {
-    this.gameService
-      .getRandomWords(randomWordRequestData)
-      .subscribe((res) => console.warn(res));
+    this.gameService.getRandomWords(randomWordRequestData).subscribe((res) => {
+      if (!res.isError) {
+        this.listOfWords = res.wordList;
+      } else {
+        this.errorMessage = res.message;
+      }
+    });
   }
 }
