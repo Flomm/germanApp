38412bde3058e14d73aa9ea4a73690a0fd741fab
baranyaf <baranya.florian@molaris.hu>
2022-01-09 21:33:49 +0100

    Info gets index by input

--- frontend/src/app/features/consumer/consumer-game/info/info.component.ts
+++ frontend/src/app/features/consumer/consumer-game/info/info.component.ts
@@ -1,25 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { GameService } from 'src/app/core/services/gameService/game.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
 })
-export class InfoComponent implements OnInit {
-  @Input() numOfQuestions: number = 0;
-  indexSub: Subscription;
-  index: number;
-
-  constructor(private gameService: GameService) {}
-
-  ngOnInit(): void {
-    this.indexSub = this.gameService.actualIndexObservable.subscribe((res) => {
-      this.index = res;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.indexSub.unsubscribe();
-  }
+export class InfoComponent {
+  @Input() numOfQuestions: number;
+  @Input() index: number;
 }
