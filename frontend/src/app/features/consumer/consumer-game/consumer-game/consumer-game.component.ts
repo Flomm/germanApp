b7195fe44aa3c78700091bd1f93d788e09adcd6d
baranyaf <baranya.florian@molaris.hu>
2022-01-09 22:37:40 +0100

    Playcard functionality works WIP

--- frontend/src/app/features/consumer/consumer-game/consumer-game/consumer-game.component.ts
+++ frontend/src/app/features/consumer/consumer-game/consumer-game/consumer-game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from 'src/app/core/services/gameService/game.service';
+import { Language } from 'src/app/shared/models/enums/Language.enum';
 import IGetWordData from 'src/app/shared/models/models/viewModels/IGetWordData.viewModel';
 import IGetRandomWordRequest from 'src/app/shared/models/requests/IGetRandomWordRequest';
 
@@ -11,7 +12,9 @@ import IGetRandomWordRequest from 'src/app/shared/models/requests/IGetRandomWord
 export class ConsumerGameComponent implements OnInit {
   isGameOn = false;
   listOfWords: IGetWordData[] = [];
+  actualIndex: number;
   errorMessage: string;
+  language: Language;
 
   constructor(private gameService: GameService) {}
 
@@ -20,11 +23,19 @@ export class ConsumerGameComponent implements OnInit {
   onGetRandomWords(randomWordRequestData: IGetRandomWordRequest): void {
     this.gameService.getRandomWords(randomWordRequestData).subscribe((res) => {
       if (!res.isError) {
+        this.actualIndex = 0;
         this.listOfWords = res.wordList;
+        this.language = randomWordRequestData.language;
         this.isGameOn = true;
       } else {
         this.errorMessage = res.message;
       }
     });
+  }
+
+  goToNextWord(): void {
+    if (this.actualIndex < this.listOfWords.length - 1) {
+      this.actualIndex++;
+    }
   }
 }
--- frontend/src/app/features/consumer/consumer-game/play-card/play-card.component.ts
+++ frontend/src/app/features/consumer/consumer-game/play-card/play-card.component.ts
@@ -1,29 +1,61 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Gender } from 'src/app/shared/models/enums/Gender.enum';
 import { Language } from 'src/app/shared/models/enums/Language.enum';
 import IGetWordData from 'src/app/shared/models/models/viewModels/IGetWordData.viewModel';
-import IGetRandomWordRequest from 'src/app/shared/models/requests/IGetRandomWordRequest';
 
 @Component({
   selector: 'app-play-card',
   templateUrl: './play-card.component.html',
   styleUrls: ['./play-card.component.scss'],
 })
-export class PlayCardComponent implements OnInit {
+export class PlayCardComponent implements OnInit, OnChanges {
   @Input() actualWord: IGetWordData;
   @Input() language: Language;
 
-  @Output() randomWordRequest: EventEmitter<IGetRandomWordRequest> =
-    new EventEmitter<IGetRandomWordRequest>();
-  wordForm: FormGroup = new FormGroup({});
+  @Output() nextEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  ngOnInit(): void {}
+  wordForm: FormArray;
+  languageType = Language;
+  genderType = Gender;
+  isSubmitted: boolean;
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  ngOnChanges(): void {
+    this.isSubmitted = false;
+    this.createForm();
+  }
 
   createForm(): void {
-    this.wordForm = new FormGroup({});
+    this.wordForm = new FormArray([]);
+    for (let i: number = 0; i <= this.actualWord.numOfTranslations - 1; i++) {
+      const fg: FormGroup = new FormGroup({
+        answer: new FormControl('', i === 0 ? Validators.required : null),
+      });
+      if (this.language === Language.HU) {
+        fg.addControl('gender', new FormControl(''));
+      }
+      this.wordForm.push(fg);
+    }
+  }
+
+  emitNext(): void {
+    this.wordForm.reset();
+    this.nextEmitter.emit(true);
   }
 
   submitForm(): void {
-    this.randomWordRequest.emit(this.wordForm.value);
+    console.warn('submit');
+    this.isSubmitted = true;
   }
 }
