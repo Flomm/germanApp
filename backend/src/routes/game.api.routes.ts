70fe0627919bcd1b4f875a5fbdf012f412076fed
baranyaf <baranya.florian@molaris.hu>
2022-01-10 20:56:57 +0100

    Add addword EP

--- backend/src/routes/game.api.routes.ts
+++ backend/src/routes/game.api.routes.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import { gameController } from '../controllers/gameController/gameController';
 import tokenAuthentication from '../middlewares/jwtAuthenticator/jwtAuthenticator';
 import permitChecker from '../middlewares/permitChecker/permitChecker';
-import { queryValidator } from '../middlewares/requestValidator/requestValidator';
+import {
+  bodyValidator,
+  queryValidator,
+} from '../middlewares/requestValidator/requestValidator';
 import { UserRole } from '../models/models/Enums/UserRole.enum';
 
 export const gameRouter = express.Router();
@@ -42,6 +45,8 @@ export const gameRouter = express.Router();
  *          description: Unauthorized
  *        '403':
  *          description: User is not a consumer
+ *        '404':
+ *          description: Invalid language or body is not complete
  *        '500':
  *          description: Internal server error
  * definitions:
@@ -63,3 +68,78 @@ gameRouter
     queryValidator(['quantity']),
   )
   .get(gameController.getRandomWords);
+
+/**
+ * @swagger
+ * paths:
+ *  /api/check-answer/{lang}:
+ *    post:
+ *      tags:
+ *        - GAME
+ *      summary: Check if answer is correct as consumer
+ *      security:
+ *        - Bearer: []
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *      - in: path
+ *        name: lang
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: Language
+ *      - in: body
+ *        name: body
+ *        description: Answer details
+ *        required: true
+ *        schema:
+ *          $ref: '#/definitions/CheckAnswerRequest'
+ *      responses:
+ *        '200':
+ *          description: Successfully checked answer
+ *        '401':
+ *          description: Unauthorized
+ *        '403':
+ *          description: User is not a consumer
+ *        '404':
+ *          description: Invalid language or body is not complete
+ *        '500':
+ *          description: Internal server error
+ * definitions:
+ *  CheckAnswerRequest:
+ *      type: object
+ *      properties:
+ *          wordId:
+ *              type: integer
+ *          answerList:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  answer:
+ *                    type: string
+ *                  gender:
+ *                    type: string
+ *  CheckAnswerResponse:
+ *      type: object
+ *      properties:
+ *          wordId:
+ *              type: integer
+ *          answerList:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  answer:
+ *                    type: string
+ *                  gender:
+ *                    type: string
+ */
+gameRouter
+  .route('/check-answer/:lang')
+  .all(
+    tokenAuthentication(),
+    permitChecker([UserRole.Consumer]),
+    bodyValidator(['wordId', 'answerList']),
+  )
+  .post();
