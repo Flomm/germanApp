d71b4534db51d008fcc98bf8167d269817c37fe5
baranyaf <baranya.florian@molaris.hu>
2022-01-12 20:22:35 +0100

    Add tests for post check answers

--- backend/src/controllers/gameController/gameController.test.ts
+++ backend/src/controllers/gameController/gameController.test.ts
@@ -4,6 +4,8 @@ import IGetWordsDataModel from '../../models/models/dataModels/IGetWordsDataMode
 import { gameService } from '../../services/gameService/gameService';
 import { jwtService } from '../../services/jwtService/jwt.service';
 import { serverError } from '../../services/errorCreatorService/errorCreator.service';
+import ICheckAnswerResponse from '../../models/responses/ICheckAnswerResponse';
+import ICheckAnswerRequest from '../../models/requests/ICheckAnswerRequest';
 
 const mockDeWords: IGetWordsDataModel[] = [
   {
@@ -18,6 +20,16 @@ const mockHunWords: IGetWordsDataModel[] = [
     word: 'víz',
   },
 ];
+
+const mockCheckAnswerRequest: ICheckAnswerRequest = {
+  wordId: 1,
+  answerList: [{ answer: 'test' }],
+};
+
+const mockCheckAnswerResponse: ICheckAnswerResponse = {
+  isCorrect: true,
+  translations: [{ translation: 'test' }],
+};
 
 const token: string =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjUsImVtYWlsIjoiZmJnZXJhem9sQGdtYWlsLmNvbSIsInJvbGVJZCI6MiwiaWF0IjoxNjQxMzE1NDM5LCJleHAiOjE2NDEzMTkwMzl9.c2w8OdrzlLLJ5cS54dbL9fyM_Kf56s2AZGvX2O_xt94';
@@ -103,5 +115,79 @@ describe('GET /random-words', () => {
     expect(response.statusCode).toEqual(500);
     expect(response.body).toEqual({ message: 'test' });
     expect(gameService.getRandomWords).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('POST /check-answers', () => {
+  test('succesfully checked an answer for German word', async () => {
+    //Arrange
+    gameService.checkAnswer = jest
+      .fn()
+      .mockResolvedValue(mockCheckAnswerResponse);
+    jwtService.verifyToken = jest.fn().mockResolvedValue(true);
+
+    //Act
+    const response = await request(app)
+      .post('/api/game/check-answer/de')
+      .set({ authorization: `Bearer ${token}` })
+      .send(mockCheckAnswerRequest);
+
+    //Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual(mockCheckAnswerResponse);
+    expect(gameService.checkAnswer).toHaveBeenCalledTimes(1);
+  });
+
+  test('succesfully checked an answer for Hungarian word', async () => {
+    //Arrange
+    gameService.checkAnswer = jest
+      .fn()
+      .mockResolvedValue(mockCheckAnswerResponse);
+    jwtService.verifyToken = jest.fn().mockResolvedValue(true);
+
+    //Act
+    const response = await request(app)
+      .post('/api/game/check-answer/hu')
+      .set({ authorization: `Bearer ${token}` })
+      .send(mockCheckAnswerRequest);
+
+    //Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual(mockCheckAnswerResponse);
+    expect(gameService.checkAnswer).toHaveBeenCalledTimes(1);
+  });
+
+  test('invalid language', async () => {
+    //Arrange
+    console.error = jest.fn();
+
+    //Act
+    const response = await request(app)
+      .post('/api/game/check-answer/test')
+      .set({ authorization: `Bearer ${token}` })
+      .send(mockCheckAnswerRequest);
+
+    //Assert
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({
+      message: 'Nincs ilyen nyelv a szótárban.',
+    });
+  });
+
+  test('error in the service', async () => {
+    //Arrange
+    gameService.checkAnswer = jest.fn().mockRejectedValue(serverError('test'));
+    console.error = jest.fn();
+
+    //Act
+    const response = await request(app)
+      .post('/api/game/check-answer/de')
+      .set({ authorization: `Bearer ${token}` })
+      .send(mockCheckAnswerRequest);
+
+    //Assert
+    expect(response.statusCode).toEqual(500);
+    expect(response.body).toEqual({ message: 'test' });
+    expect(gameService.checkAnswer).toHaveBeenCalledTimes(1);
   });
 });
