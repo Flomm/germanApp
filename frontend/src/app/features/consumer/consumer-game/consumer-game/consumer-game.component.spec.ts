d2529b07170b797d8d10d402f38c4ed145ec6477
baranyaf <baranya.florian@molaris.hu>
2022-01-16 14:12:26 +0100

    Add import

--- frontend/src/app/features/consumer/consumer-game/consumer-game/consumer-game.component.spec.ts
+++ frontend/src/app/features/consumer/consumer-game/consumer-game/consumer-game.component.spec.ts
@@ -1,5 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { GameService } from 'src/app/core/services/gameService/game.service';
 
 import { ConsumerGameComponent } from './consumer-game.component';
@@ -12,7 +14,7 @@ describe('ConsumerGameComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ConsumerGameComponent],
       providers: [{ provide: GameService }],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, MatDialogModule, MatSnackBarModule],
     }).compileComponents();
   });
 
