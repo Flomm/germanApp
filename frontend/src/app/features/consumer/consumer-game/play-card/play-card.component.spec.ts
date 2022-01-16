10b6596c260cf7d51249b9cf1257a0cc46572ba8
baranyaf <baranya.florian@molaris.hu>
2022-01-16 11:14:26 +0100

    Add tests for emitters

--- frontend/src/app/features/consumer/consumer-game/play-card/play-card.component.spec.ts
+++ frontend/src/app/features/consumer/consumer-game/play-card/play-card.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormArray } from '@angular/forms';
 import { PlayCardComponent } from './play-card.component';
 
 describe('PlayCardComponent', () => {
@@ -14,10 +15,36 @@ describe('PlayCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlayCardComponent);
     component = fixture.componentInstance;
+    component.actualWord = { id: 1, word: 'test', numOfTranslations: 1 };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit true when next submitted', () => {
+    //Arrange
+    spyOn(component.nextEmitter, 'emit');
+
+    //Act
+    component.emitNext();
+
+    //Assert
+    expect(component.nextEmitter.emit).toHaveBeenCalledWith(true);
+  });
+
+  it('should emit the expected values and format when word request is submitted', () => {
+    //Arrange
+    spyOn(component.answerEmitter, 'emit');
+    component.wordForm.controls.answerList.setValue([{ answer: 'test' }]);
+
+    //Act
+    component.submitForm();
+
+    //Assert
+    expect(component.answerEmitter.emit).toHaveBeenCalledWith([
+      { answer: 'test' },
+    ]);
   });
 });
