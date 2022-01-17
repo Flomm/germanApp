9d6b850fbd5876d37d2c755b3e7c044ae2bfe2bf
baranyaf <baranya.florian@molaris.hu>
2022-01-17 21:13:51 +0100

    Create finish screen comp

--- frontend/src/app/features/consumer/consumer-game/finish-screen/finish-screen.component.ts
+++ frontend/src/app/features/consumer/consumer-game/finish-screen/finish-screen.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import IGetWordData from 'src/app/shared/models/models/viewModels/IGetWordData.viewModel';
 
 @Component({
   selector: 'app-finish-screen',
   templateUrl: './finish-screen.component.html',
   styleUrls: ['./finish-screen.component.scss'],
 })
-export class FinishScreenComponent implements OnInit {
-  constructor() {}
+export class FinishScreenComponent {
+  @Input() resultString: string;
+  @Input() incorrectWords: IGetWordData[] = [];
 
-  ngOnInit(): void {}
+  @Output() decisionEmitter: EventEmitter<boolean> =
+    new EventEmitter<boolean>();
+
+  emitDecisionSignal(resetOrReplay: boolean): void {
+    this.decisionEmitter.emit(resetOrReplay);
+  }
 }
