edda66178ea02a532ba7ef4094f1bb043533f91d
baranyaf <baranya.florian@molaris.hu>
2022-01-18 22:16:56 +0100

    Add snackbar to the tests

--- frontend/src/app/core/interceptors/expiredTokenInterceptor/expiredToken.interceptor.spec.ts
+++ frontend/src/app/core/interceptors/expiredTokenInterceptor/expiredToken.interceptor.spec.ts
@@ -10,35 +10,45 @@ import {
 } from '@angular/common/http';
 import ExpiredTokenInterceptor from './expiredToken.interceptor';
 import { of, throwError } from 'rxjs';
+import { MessageService } from '../../services/messageService/message.service';
 
 describe(`ExpiredTokenInterceptor`, () => {
   let authServiceSpy: jasmine.SpyObj<AuthService>;
   let httpRequestSpy: jasmine.SpyObj<HttpRequest<any>>;
   let httpHandlerSpy: jasmine.SpyObj<HttpHandler>;
+  let messageServiceSpy: jasmine.SpyObj<MessageService>;
   let expiredTokenInterceptor: ExpiredTokenInterceptor;
 
   beforeEach(() => {
     authServiceSpy = jasmine.createSpyObj<AuthService>('AuthService', [
       'logout',
     ]);
+    messageServiceSpy = jasmine.createSpyObj<MessageService>('MessageService', [
+      'openSnackBar',
+    ]);
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
         AuthService,
+        MessageService,
         {
           provide: HTTP_INTERCEPTORS,
           useClass: ExpiredTokenInterceptor,
           multi: true,
         },
         { provide: AuthService, useValue: authServiceSpy },
+        { provide: MessageService, useValue: messageServiceSpy },
       ],
     });
     httpRequestSpy = jasmine.createSpyObj('HttpRequest', ['notImportant']);
     httpHandlerSpy = jasmine.createSpyObj('HttpHandler', ['handle']);
-    expiredTokenInterceptor = new ExpiredTokenInterceptor(authServiceSpy);
+    expiredTokenInterceptor = new ExpiredTokenInterceptor(
+      authServiceSpy,
+      messageServiceSpy
+    );
   });
 
-  it('should call the logout method of the AuthService when response is with the adequate error', () => {
+  it('should call the logout method of the AuthService and open the correct snackbar when response is with the adequate error', () => {
     // Arrange
     const mockHttpError: Partial<HttpErrorResponse> = {
       status: 401,
@@ -54,9 +64,14 @@ describe(`ExpiredTokenInterceptor`, () => {
 
     //Assert
     expect(authServiceSpy.logout).toHaveBeenCalledTimes(1);
+    expect(messageServiceSpy.openSnackBar).toHaveBeenCalledOnceWith(
+      'Nincs érvényes token.',
+      '',
+      { panelClass: ['warn'] }
+    );
   });
 
-  it('should not call the logout method of the AuthService when response is with not a triggering error', () => {
+  it('should not call the logout method of the AuthService and should not open snackbar when response is with not a triggering error', () => {
     // Arrange
     const mockHttpError: Partial<HttpErrorResponse> = {
       status: 404,
@@ -72,9 +87,10 @@ describe(`ExpiredTokenInterceptor`, () => {
 
     //Assert
     expect(authServiceSpy.logout).not.toHaveBeenCalled();
+    expect(messageServiceSpy.openSnackBar).not.toHaveBeenCalled();
   });
 
-  it('should not call the logout method of the AuthService when response is not errorous', () => {
+  it('should not call the logout method of the AuthService and should not open snackbar when response is not errorous', () => {
     // Arrange
     const mockResponseObject = new HttpResponse({
       status: 200,
@@ -90,5 +106,6 @@ describe(`ExpiredTokenInterceptor`, () => {
 
     //Assert
     expect(authServiceSpy.logout).not.toHaveBeenCalled();
+    expect(messageServiceSpy.openSnackBar).not.toHaveBeenCalled();
   });
 });
