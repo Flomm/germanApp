f5ae72fbae188f4c7f8612321d535e675bdb805c
baranyaf <baranya.florian@molaris.hu>
2022-01-22 16:30:44 +0100

    Add topic

--- frontend/src/app/shared/models/requests/IAddWordRequest.ts
+++ frontend/src/app/shared/models/requests/IAddWordRequest.ts
@@ -1,8 +1,10 @@
 import { Gender } from '../enums/Gender.enum';
+import { TopicType } from '../enums/TopicType.enum';
 import ITranslationDataModel from './ITranslationDataModel';
 
 export default interface IAddWordRequest {
   word: string;
   translations: ITranslationDataModel[];
+  topic: TopicType;
   gender?: Gender;
 }
