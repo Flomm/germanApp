b370da12c26bc7512ce3a21f62527feaac5dfae4
baranyaf <baranya.florian@molaris.hu>
2022-01-23 21:37:20 +0100

    Add value multiselect

--- frontend/src/app/features/consumer/consumer-game/main-menu/main-menu.component.ts
+++ frontend/src/app/features/consumer/consumer-game/main-menu/main-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Language } from 'src/app/shared/models/enums/Language.enum';
+import { TopicType } from 'src/app/shared/models/enums/TopicType.enum';
 import IGetRandomWordRequest from 'src/app/shared/models/requests/IGetRandomWordRequest';
 
 @Component({
@@ -13,11 +14,13 @@ export class MainMenuComponent implements OnInit {
     new EventEmitter<IGetRandomWordRequest>();
   mainMenuForm: FormGroup;
   languageType = Language;
+  topicType = TopicType;
   quantityArray: number[] = [10, 25, 50];
 
   ngOnInit(): void {
     this.mainMenuForm = new FormGroup({
       language: new FormControl(Language.DE, Validators.required),
+      topic: new FormControl(TopicType.FAMILY, [Validators.required]),
       quantity: new FormControl(10, Validators.required),
     });
   }
