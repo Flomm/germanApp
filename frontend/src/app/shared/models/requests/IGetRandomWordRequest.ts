d04854e6b95fb39d615e67b1bf1b4077669d94b5
baranyaf <baranya.florian@molaris.hu>
2022-01-26 11:43:47 +0100

    Add topic

--- frontend/src/app/shared/models/requests/IGetRandomWordRequest.ts
+++ frontend/src/app/shared/models/requests/IGetRandomWordRequest.ts
@@ -1,6 +1,8 @@
 import { Language } from '../enums/Language.enum';
+import { TopicType } from '../enums/TopicType.enum';
 
 export default interface IGetRandomWordRequest {
   quantity: number;
   language: Language;
+  topic: TopicType;
 }
