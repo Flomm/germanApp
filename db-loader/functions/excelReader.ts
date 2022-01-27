3802db0d2960f19d8e06459065c9c077d0847810
baranyaf <baranya.florian@molaris.hu>
2022-01-27 21:35:29 +0100

    Add excelReader

---
+++ db-loader/functions/excelReader.ts
@@ -0,0 +1,14 @@
+import xlsx from "node-xlsx";
+import * as fs from "fs";
+import IExcelObjectModel from "../models/IexcelObjectModel";
+
+export const excelReader = (fileName: string): IExcelObjectModel[] => {
+  try {
+    const excelObject: IExcelObjectModel[] = xlsx.parse(
+      fs.readFileSync(`./testFiles/${fileName}.xlsx`)
+    );
+    return excelObject;
+  } catch (error) {
+    console.log(`An error occured: ${error.message}`);
+  }
+};
