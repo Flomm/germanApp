0f1bb83af35066b9438d3fa916a6f50f4a2ddab2
baranyaf <baranya.florian@molaris.hu>
2022-01-29 14:38:58 +0100

    Add missing imports

--- frontend/src/app/features/admin/admin-modify-word/admin-modify-word/admin-modify-word.component.spec.ts
+++ frontend/src/app/features/admin/admin-modify-word/admin-modify-word/admin-modify-word.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Language } from 'src/app/shared/models/enums/Language.enum';
+import { TopicType } from 'src/app/shared/models/enums/TopicType.enum';
+import EnumToViewPipe from 'src/app/shared/pipes/enumToView/enumToView.pipe';
+import TranslationPipe from 'src/app/shared/pipes/translationPipe/translation.pipe';
 import { AdminModifyWordComponent } from './admin-modify-word.component';
 
 describe('AdminModifyWordComponent', () => {
@@ -8,13 +11,22 @@ describe('AdminModifyWordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AdminModifyWordComponent],
+      declarations: [AdminModifyWordComponent, EnumToViewPipe, TranslationPipe],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AdminModifyWordComponent);
     component = fixture.componentInstance;
+    component.modifyWordData = {
+      initRequest: {
+        word: 'test',
+        wordId: 1,
+        language: Language.DE,
+        topic: TopicType.FAMILY,
+      },
+      translationList: [],
+    };
     fixture.detectChanges();
   });
 
