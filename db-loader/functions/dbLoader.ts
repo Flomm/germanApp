864831ba64e18f4cb10f4202b16353a92f31272c
baranyaf <baranya.florian@molaris.hu>
2022-01-31 21:48:34 +0100

    dbLoader works

--- db-loader/functions/dbLoader.ts
+++ db-loader/functions/dbLoader.ts
@@ -1,19 +1,27 @@
-import { db } from "../data/connection";
 import IAddWordDataModel from "../models/IAddWordDataModel";
 import IExcelObjectModel from "../models/IExcelObjectModel";
+import { wordRepository } from "./addWordRepo";
 import { excelReader } from "./excelReader";
 import { rowToObjectTransformer } from "./rowToObjectTransformer";
 
 export const dbLoader = (fileName: string, language: string): void => {
-  db.checkConnection()
-    .then(() => {
-      const excelFileData: IExcelObjectModel[] = excelReader(fileName);
-      const wordObjectList: IAddWordDataModel[][] = rowToObjectTransformer(
-        excelFileData,
-        language
-      );
-    })
-    .catch((err) => {
-      console.log(err.message);
+  try {
+    console.log(`Start loading ${language} words...`);
+    const excelFileData: IExcelObjectModel[] = excelReader(fileName);
+    const wordObjectList: IAddWordDataModel[][] = rowToObjectTransformer(
+      excelFileData,
+      language
+    );
+    wordObjectList.forEach((wordArray) => {
+      if (wordArray.length > 0) {
+        wordArray.forEach((wordObj) => {
+          wordRepository
+            .addNewWordEntry(language, wordObj)
+            .catch((err) => Promise.reject(err));
+        });
+      }
     });
+  } catch (error) {
+    throw error;
+  }
 };
