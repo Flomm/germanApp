7ee78a6aab1d78d4c2bc3dd8b178ae799fcc5034
baranyaf <baranya.florian@molaris.hu>
2022-01-31 21:35:13 +0100

    Fix ?

--- db-loader/functions/addWordRepo.ts
+++ db-loader/functions/addWordRepo.ts
@@ -5,10 +5,7 @@ import IAddWordDataModel from "../models/IAddWordDataModel";
 export const wordRepository = {
   getWordByWord(lang: string, word: string): Promise<any> {
     return db
-      .query<any[]>(`SELECT * FROM german_app.?? WHERE word = ?`, [
-        `${lang}`,
-        word,
-      ])
+      .query<any[]>(`SELECT * FROM german_app.${lang} WHERE word = ?`, [word])
       .then((res) => res[0])
       .catch((err) => Promise.reject(err));
   },
@@ -16,7 +13,7 @@ export const wordRepository = {
   getWordById(lang: string, wordId: number): Promise<any> {
     return db
       .query<any[]>(
-        `SELECT * FROM german_app.?? WHERE id = ? AND isDeleted = 0`,
+        `SELECT * FROM german_app.${lang} WHERE id = ? AND isDeleted = 0`,
         [`${lang}`, `${wordId}`]
       )
       .then((res) => res[0])
@@ -25,7 +22,7 @@ export const wordRepository = {
 
   async addWord(lang: string, newWord: IAddWordDataModel): Promise<any> {
     try {
-      let queryString: string = `UPDATE german_app.?? SET isDeleted = 0 WHERE id = ?`;
+      let queryString: string = `UPDATE german_app.${lang} SET isDeleted = 0 WHERE id = ?`;
       const existingWord: any = await wordRepository.getWordByWord(
         lang,
         newWord.word
@@ -33,7 +30,6 @@ export const wordRepository = {
       if (existingWord) {
         if (existingWord.isDeleted) {
           const dbResult: any = await db.query<any>(queryString, [
-            `${lang}`,
             `${existingWord.id}`,
           ]);
           if (dbResult.affectedRows === 0) {
@@ -45,16 +41,15 @@ export const wordRepository = {
         }
       } else {
         let queryArray: string[] = [
-          `${lang}`,
           newWord.word,
           `${newWord.translations.length}`,
           `${newWord.topic}`,
         ];
         if (lang === "de" && newWord.gender) {
-          queryString = `INSERT INTO german_app.?? (word, numOfTranslations, topic, gender) VALUES (?, ?, ?, ?)`;
+          queryString = `INSERT INTO german_app.${lang} (word, numOfTranslations, topic, gender) VALUES (?, ?, ?, ?)`;
           queryArray.push(newWord.gender);
         } else {
-          queryString = `INSERT INTO german_app.?? (word, numOfTranslations, topic) VALUES (?, ?)`;
+          queryString = `INSERT INTO german_app.${lang} (word, numOfTranslations, topic) VALUES (?, ?, ?)`;
         }
         const dbResult: any = await db.query<any>(queryString, queryArray);
         if (dbResult.affectedRows === 0) {
