4534eae8be679ac776cce6f1fd8189d0aee22aa7
baranyaf <baranya.florian@molaris.hu>
2022-01-31 21:48:45 +0100

    update exe file

--- db-loader/exe.ts
+++ db-loader/exe.ts
@@ -1,3 +1,12 @@
+import { db } from "./data/connection";
 import { dbLoader } from "./functions/dbLoader";
 
-dbLoader("german", "hu");
+db.checkConnection()
+  .then(() => {
+    dbLoader("main_hu", "hu");
+    dbLoader("main_de", "de");
+  })
+  .catch((err) => {
+    console.log(err.message);
+    db.disconnect();
+  });
