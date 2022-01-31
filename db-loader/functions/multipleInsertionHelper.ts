a84d635fdf8e94fc2fddeb8e3d0cb6c16023070d
baranyaf <baranya.florian@molaris.hu>
2022-01-31 21:11:52 +0100

    add helper

---
+++ db-loader/functions/multipleInsertionHelper.ts
@@ -0,0 +1,16 @@
+export function generateMultipleInsertQueryQuestionMarks(
+  numberOfQuestionMarks: number,
+  numberOfItems: number
+): string {
+  return `${generateQuestionMarksForOneItem(
+    numberOfQuestionMarks
+  )}${`, ${generateQuestionMarksForOneItem(numberOfQuestionMarks)}`.repeat(
+    numberOfItems - 1
+  )}`;
+}
+
+function generateQuestionMarksForOneItem(
+  numberOfQuestionMarks: number
+): string {
+  return `(?${",?".repeat(numberOfQuestionMarks - 1)})`;
+}
