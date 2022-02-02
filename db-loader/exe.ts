dbb9868405378355603a85a5c2d83474bbc540e5
baranyaf <baranya.florian@molaris.hu>
2022-02-02 20:55:46 +0100

    exe only contains a call of the main execution fun

--- db-loader/exe.ts
+++ db-loader/exe.ts
@@ -1,16 +1,3 @@
-import { db } from "./data/connection";
-import { dbLoader } from "./functions/dbLoader";
+import { mainExec } from "./functions/mainExec";
 
-db.checkConnection().then(() => {
-  // dbLoader("main_hu", "hu").catch((err) => Promise.reject(err));
-  dbLoader("main_de2q2e", "de")
-    .then((_) => {
-      console.log("Closing connection");
-      db.disconnect();
-    })
-    .catch((err) => {
-      console.warn(err.message);
-      console.log("Closing connection due to error");
-      db.disconnect();
-    });
-});
+mainExec(["main_hu", "main_de"]);
