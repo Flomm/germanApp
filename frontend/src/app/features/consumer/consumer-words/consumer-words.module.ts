eb272bf2863e8fb1f67c9c1dd6831e3643e3c820
baranyaf <baranya.florian@molaris.hu>
2022-02-12 08:39:55 +0100

    Wire in word table for user

--- frontend/src/app/features/consumer/consumer-words/consumer-words.module.ts
+++ frontend/src/app/features/consumer/consumer-words/consumer-words.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConsumerWordsComponent } from './consumer-words/consumer-words.component';
 import { ConsumerWordsRoutingModule } from './consumer-words-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ConsumerWordsComponent],
-  imports: [CommonModule, ConsumerWordsRoutingModule],
+  imports: [CommonModule, ConsumerWordsRoutingModule, SharedModule],
 })
 export class ConsumerWordsModule {}
