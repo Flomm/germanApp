22070b9f76de2a86d269e965b4b4fe6b1c102521
baranyaf <baranya.florian@molaris.hu>
2022-02-13 09:56:00 +0100

    Add translation pipe for user enum

--- frontend/src/app/core/services/enumService/enum.service.ts
+++ frontend/src/app/core/services/enumService/enum.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TopicTypeTranslation } from 'src/app/shared/models/enums/TopicTypeTranslation.enum';
+import { UserRoleTranslation } from 'src/app/shared/models/enums/UserRoleTranslation.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +8,10 @@ import { TopicTypeTranslation } from 'src/app/shared/models/enums/TopicTypeTrans
 export class EnumService {
   translateTopicType(value: number | string): string {
     return TopicTypeTranslation[value] || 'N/A';
+  }
+
+  translateUserType(value: number | string): string {
+    console.warn(value);
+    return UserRoleTranslation[value] || 'N/A';
   }
 }
---
+++ frontend/src/app/shared/models/enums/UserRoleTranslation.enum.ts
@@ -0,0 +1,5 @@
+export enum UserRoleTranslation {
+  Admin = 'Admin',
+  Consumer = 'Felhasználó',
+  All = 'Összes',
+}
