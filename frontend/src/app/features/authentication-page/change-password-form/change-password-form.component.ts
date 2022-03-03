2fe65a54518fcdb2ba0db8740348d0f7a4c6e37e
baranyaf <baranya.florian@molaris.hu>
2022-03-03 17:54:24 +0100

    Create change-password form

---
+++ frontend/src/app/features/authentication-page/change-password-form/change-password-form.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password-form',
+  templateUrl: './change-password-form.component.html',
+})
+export class ChangePasswordFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
