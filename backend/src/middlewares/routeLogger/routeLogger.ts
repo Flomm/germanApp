fae924fdfb0bb42541548fdd1f7338a1db604e5b
baranyaf <baranya.florian@molaris.hu>
2022-03-14 14:18:09 +0100

    Create logger mw

--- backend/src/middlewares/routeLogger/routeLogger.ts
+++ backend/src/middlewares/routeLogger/routeLogger.ts
@@ -4,4 +4,11 @@ export default function routeLogger(
   req: Request,
   res: Response,
   next: NextFunction,
-) {}
+): void {
+  const currentTime = new Date();
+  const formattedDate = `${currentTime.getFullYear()}-${currentTime.getMonth()}-${currentTime.getDate()} ${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
+  console.log(
+    `[${formattedDate}] ${req.originalUrl} - ${req.method} - ${req.ip}`,
+  );
+  next();
+}
