64176d512aab4f09671574d9fbc99d88c4e3f6b2
baranyaf <baranya.florian@molaris.hu>
2022-04-07 20:27:01 +0200

    Use env var NODE_ENV as param

--- db-loader/resetWords.exe.ts
+++ db-loader/resetWords.exe.ts
@@ -1,4 +1,4 @@
 import { mainExec } from "./functions/mainExec";
 import { EnvType } from "./models/EnvType.enum";
 
-mainExec(["main_hu", "main_de"], false, EnvType.DEV);
+mainExec(["main_hu", "main_de"], false, process.env.NODE_ENV as EnvType);
--- db-loader/restartDB.exe.ts
+++ db-loader/restartDB.exe.ts
@@ -1,4 +1,4 @@
 import { mainExec } from "./functions/mainExec";
 import { EnvType } from "./models/EnvType.enum";
 
-mainExec(["main_hu", "main_de"], true, EnvType.DEV);
+mainExec(["main_hu", "main_de"], true, process.env.NODE_ENV as EnvType);
